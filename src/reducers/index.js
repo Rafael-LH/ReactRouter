470fa7f94cd82030ef9eefabf97554574e028833
Rafael Lopez <rafael.iskoat@gmail.com>
2020-04-12 22:43:50 -0500

    Solventamos el bug de agregar items infinitamente

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,21 @@ const reducer = (state, action) => {
   switch (action.type) {
 
     case 'SET_FAVORITE':
+      const issetList = state.myList.findIndex(item => item.id === action.payload.id);
+
+      if (issetList === -1) {
+        return {
+          ...state, // me traigo mi estado que es todo mi JSON initialState
+          myList: [ // que elemento voy actualizar que en este caso es mi lista
+            ...state.myList, //con esto me traigo play list que se agregamo más es que ya estaba agregado
+            action.payload, //y con el action.payload me traigo toda la data que mando en mi componente CarouselItem del nuevo video que agregue
+          ],
+        };
+      }
       return {
-        ...state, // me traigo mi estado que es todo mi JSON initialState
-        myList: [ // que elemento voy actualizar que en este caso es mi lista
-          ...state.myList, //con esto me traigo play list que se agregamo más es que ya estaba agregado
-          action.payload, //y con el action.payload me traigo toda la data que mando en mi componente CarouselItem del nuevo video que agregue
-        ],
+        ...state,
       };
+
       break;
 
     case 'DELETE_FAVORITE':
